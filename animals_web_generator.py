8f79d71aa68067d9334dbdcd9baf077c80a1567b
Fouad Gantri <fouad@gantri.dev>
2024-11-10 22:44:02 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,6 +2,7 @@ import json
 
 
 def main():
+    """ generates html file based on template and json data """
     animals_data = generate_animals_data()
     with open("animals_template.html", "r") as f:
         html_template = f.read().replace(
@@ -10,12 +11,12 @@ def main():
         f.write(html_template)
 
 
-
 def generate_animals_data():
+    """ Generates template string for animal data interpolation """
     output = ""
     animals_data = load_data('animals_data.json')
     for animal in animals_data:
-        output += "<li class=\"cards__item\">"
+        # get animal props if they exist else None and collect them into tuple pairs
         pairs = [("Name", animal.get("name"))]
         locations = animal.get("locations")
         if locations is not None and len(locations) > 0:
@@ -24,11 +25,18 @@ def generate_animals_data():
             pairs.append(("Diet", animal["characteristics"].get("diet")))
             pairs.append(("Type", animal["characteristics"].get("type")))
 
+        # construct template string
+        output += "<li class=\"cards__item\">"
+        _, name = pairs[0]
+        if name is not None:
+            output += f"<div class=\"card__title\">{name}</div>"
         for prop, val in pairs:
-            if val is not None:
-                output += f"{prop}: {val}<br>"
-        output += "</li>"
-    return output
+            output += "<p class=\"card__text\">"
+            if val is None:
+                continue
+            output += f"<strong>{prop}:</strong> {val}<br/>"
+
+    return output + "</p></li>"
 
 
 def load_data(file_path):
